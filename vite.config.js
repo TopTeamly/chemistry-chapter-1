d613938633670248448729cdbd2acca3132e9f70
Ameen <drak.ly.gamer@gmail.com>
2026-09-09 03:50:41 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // إعدادات Vite لمشروع أطلس - الباب الأول (الكيمياء)
 export default defineConfig({
   plugins: [react()],
+  base: '/chemistry-chapter-1/',
 })
